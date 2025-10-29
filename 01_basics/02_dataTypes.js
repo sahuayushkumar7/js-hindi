a6016a756ac04becd2d28a1fdd9e5be1098cd573
Ayush Kumar Sahu <sahuayushkumar7@gmail.com>
2025-10-29 12:56:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";
+ 
+//treat all JS code as newer version
+
+//alert(3+3) //e are using nodeJS not browser
+
+console.log(3+3)
+
+console.log("Ayush");
+
+
+let name = "Ayush"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  // object
+
